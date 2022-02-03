172bb6b45ebbfab3bc56d79161c9f42f4c27aa55
Olaisheretolearn <63797098+Olaisheretolearn@users.noreply.github.com>
2022-02-03 04:59:13 -0600

    Create QuickUnion.java

---
+++ QuickUnion.java
@@ -0,0 +1,56 @@
+import java.util.Arrays;
+public class QuickUnion{
+    private int[] id ;
+    
+    public int[] getId(){
+        return id;
+    }
+    
+    public void setId(int [] id){
+        this.id = id;
+    }
+    
+    
+    public QuickUnion(int N){
+        this.id = new int[N];
+        for (int i=0; i < N ; i++) {
+            this.id = new int[]{0,1,2,3,4,5,6,7,8,9};
+        }
+    }
+        
+        private int root(int i){
+            while (i != id[i]){
+                i = id[i];
+              
+            }
+            return i;
+        }
+            
+            public boolean connected (int p , int q){
+                return root(p) == root(q);
+            }
+            
+            public void union(int p, int q){
+                int i = root(p);
+                int j = root(q);
+                id[i] = j;
+            }
+            
+           public static void main (String[] args) {
+                QuickUnion quickunion = new QuickUnion(10);
+                System.out.println("The values in the array " + Arrays.toString(quickunion.getId()));
+                 System.out.println("Union 4, 3");
+                 quickunion.union(4,3);
+                 System.out.println("is 4 and 3 connected? " +quickunion.connected(4,3) );
+                  System.out.println("The values in the array " + Arrays.toString(quickunion.getId()));
+                  System.out.println("Union 3, 8");
+                 quickunion.union(3,8);
+                 System.out.println("is 3 and 8 connected ? " +quickunion.connected(3,8) );
+                  System.out.println("The values in the array " + Arrays.toString(quickunion.getId()));
+                  System.out.println("Union 3, 8");
+                 quickunion.union(6,5);
+                 System.out.println("is 6 and 5 connected ? " +quickunion.connected(6,5) );
+                  System.out.println("The values in the array " + Arrays.toString(quickunion.getId()));
+            }
+        }
+        
