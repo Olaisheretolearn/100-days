e65d87ebf50e46575495051778b1d8c6c58939d1
Olaisheretolearn <63797098+Olaisheretolearn@users.noreply.github.com>
2022-03-10 01:10:34 -0600

    Update SumOfDigits.java

--- SumOfDigits.java
+++ SumOfDigits.java
@@ -1,3 +1,4 @@
+
 //How to find some of digit in a positive integr
 public class Main {
   public static void main(String[] args){
@@ -6,7 +7,9 @@ public class Main {
     System.out.println(result);
   }
   public int SumOfDigits(int n){
-    /* i initially thought the base case should be  if n is less thab 10, But that would b wrong bwcause if the second round of rcursion is coming , it will give zero and that woould be like just getting the remainder, If it is two digits btw, Thre will giv the addition of remainder plus the division by 10 in the first cycle*/
+    /* i initially thought the base case should be  "if n is less than 10, But that would be wrong because if the second round of recursion is coming 
+    , it will give zero and that woould be like just getting the remainder, If it is two digits btw, 
+    Thre will giv the addition of remainder plus the division by 10 in the first cycle*/
 
     // Base case and unintentional case
     if(n==0|| n<0){
